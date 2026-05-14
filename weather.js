a40e22ba02dc798811ac48a41da603b8e1b017b4
G484072024 <248407@st.takushoku-u.ac.jp>
2026-05-14 17:59:36 +0900

    第四回

--- weather.js
+++ weather.js
@@ -15,13 +15,18 @@ function printDom(data) {
 }
 
 // 課題6-1 のイベントハンドラ登録処理は以下に記述
-
+document.querySelector('#btn')
+  .addEventListener('click', sendRequest);
 
 
 
 // 課題6-1 のイベントハンドラ sendRequest() の定義
 function sendRequest() {
+  let key =
+  document.querySelector('#key').value;
 
+
+console.log('検索キー: ' + key);
 }
 
 // 課題6-1: 通信が成功した時の処理は以下に記述
