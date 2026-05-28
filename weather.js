95bef93eaa6096b4578f90c130cb433ebbcd3aaf
G484072024 <248407@st.takushoku-u.ac.jp>
2026-05-28 16:57:13 +0900

    第六回

--- weather.js
+++ weather.js
@@ -11,47 +11,47 @@ function print(data) {
 
 // 課題5-1 の関数 printDom() はここに記述すること
 function printDom(data) {
-  let oldResult = document.querySelector('#result');
-  if (oldResult) {
-      oldResult.remove();
-  }
+
+  let result = document.querySelector('#result');
 
   
-  let divResult = document.createElement('div');
-  divResult.setAttribute('id', 'result');
+  result.innerHTML = '';
 
   
-  let body = document.querySelector('body');
-  if (body) {
-      body.insertAdjacentElement('beforeend', divResult);
-  }
+  let h2 = document.createElement('h2');
+  h2.textContent = '検索結果';
+  result.insertAdjacentElement('beforeend', h2);
 
   
   let ul = document.createElement('ul');
 
   
-  let liName = document.createElement('li');
-  liName.textContent = "都市: " + data.name;
-  ul.insertAdjacentElement('beforeend', liName);
+  let li1 = document.createElement('li');
+  li1.textContent = '都市: ' + data.name;
+  ul.insertAdjacentElement('beforeend', li1);
 
   
-  let liWeather = document.createElement('li');
-  liWeather.textContent = "天气: " + data.weather[0].description;
-  ul.insertAdjacentElement('beforeend', liWeather);
+  let li2 = document.createElement('li');
+  li2.textContent = '天気: ' + data.weather[0].description;
+  ul.insertAdjacentElement('beforeend', li2);
 
   
-  let liTemp = document.createElement('li');
-  
-  liTemp.textContent = "気温: " + data.main.temp.toFixed(1) + " °C";
-  ul.insertAdjacentElement('beforeend', liTemp);
+  let li3 = document.createElement('li');
+  li3.textContent = '気温: ' + data.main.temp.toFixed(1) + ' °C';
+  ul.insertAdjacentElement('beforeend', li3);
 
   
-  let liFeels = document.createElement('li');
-  liFeels.textContent = "体感温度: " + data.main.feels_like.toFixed(1) + " °C";
-  ul.insertAdjacentElement('beforeend', liFeels);
+  let li4 = document.createElement('li');
+  li4.textContent = '体感温度: ' + data.main.feels_like.toFixed(1) + ' °C';
+  ul.insertAdjacentElement('beforeend', li4);
 
   
-  divResult.insertAdjacentElement('beforeend', ul);
+  let li5 = document.createElement('li');
+  li5.textContent = '湿度: ' + data.main.humidity + '%';
+  ul.insertAdjacentElement('beforeend', li5);
+
+  
+  result.insertAdjacentElement('beforeend', ul);
 }
 
 // 課題6-1 のイベントハンドラ登録処理は以下に記述
@@ -62,21 +62,38 @@ document.querySelector('#btn')
 
 // 課題6-1 のイベントハンドラ sendRequest() の定義
 function sendRequest() {
-  let key =
-  document.querySelector('#key').value;
+  let key =document.querySelector('#key').value;
 
 
 console.log('検索キー: ' + key);
+let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/' + key + '.json';
+
+  
+  axios.get(url)
+      .then(showResult)   
+      .catch(showError)   
+      .then(finish);
 }
 
 // 課題6-1: 通信が成功した時の処理は以下に記述
 function showResult(resp) {
+  let data = resp.data;
 
+  
+  if (typeof data === 'string') {
+      data = JSON.parse(data);
+  }
+
+  
+  printDom(data);
 }
 
 // 課題6-1: 通信エラーが発生した時の処理
 function showError(err) {
     console.log(err);
+    let result = document.querySelector('#result');
+
+    result.innerHTML = '<p>検索に失敗しました</p>';
 }
 
 // 課題6-1: 通信の最後にいつも実行する処理
@@ -88,50 +105,3 @@ function finish() {
 // 以下はグルメのデータサンプル
 // 注意: 第5回までは以下を変更しないこと！
 // 注意2: 課題6-1 で以下をすべて削除するこ
-window.data = {
-  "coord": {
-    "lon": 116.3972,
-    "lat": 39.9075
-  },
-  "weather": [
-    {
-      "id": 803,
-      "main": "Clouds",
-      "description": "曇りがち",
-      "icon": "04d"
-    }
-  ],
-  "base": "stations",
-  "main": {
-    "temp": 9.94,
-    "feels_like": 8.65,
-    "temp_min": 9.94,
-    "temp_max": 9.94,
-    "pressure": 1022,
-    "humidity": 14,
-    "sea_level": 1022,
-    "grnd_level": 1016
-  },
-  "visibility": 10000,
-  "wind": {
-    "speed": 2.65,
-    "deg": 197,
-    "gust": 4.84
-  },
-  "clouds": {
-    "all": 53
-  },
-  "dt": 1646542386,
-  "sys": {
-    "type": 1,
-    "id": 9609,
-    "country": "CN",
-    "sunrise": 1646520066,
-    "sunset": 1646561447
-  },
-  "timezone": 28800,
-  "id": 1816670,
-  "name": "北京市",
-  "cod": 200
-};
-
